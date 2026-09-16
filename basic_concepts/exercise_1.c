fe3c90656d9441f53c57ccf685e1ea1bfaae571a
Pablo Oliva <pabloadra1@gmail.com>
2026-09-16 21:48:51 +0200

    New file

---
+++ basic_concepts/exercise_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+/*Es un preprocesador. Le decimos al compilador que incluya las entradas/salidas estandar. 
+Contiene mucha información. Si quieres imprimir algo por pantalla lo necesitas.*/
+
+int main (){
+
+    printf ("I like pizza\n");
+    printf ("It's really good");
+    return 0;
+}
